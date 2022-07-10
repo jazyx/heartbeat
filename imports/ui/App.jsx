22662015c3d8a8f5e80fce289a8ed910ded5cdda
James Newton <blackslate@lexogram.com>
2022-07-10 17:50:58 +0300

    Proof of concept demo of heartbeat feature

---
+++ imports/ui/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Heartbeat } from './components/Heartbeat'
+
+export const App = () => (
+  <div>
+    <Heartbeat />
+  </div>
+);
---
+++ server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import '/imports/api/collections/heartbeat';
+import '/imports/api/methods/heartbeat';
+
+
+
+Meteor.startup(() => {
+});
